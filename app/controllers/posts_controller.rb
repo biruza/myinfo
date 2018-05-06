d2039488d9dea0235ec6836a60c8ab935103b604
phada <pwongbeasujj@gmail.com>
2018-05-06 20:16:34 +0700

    add upload image post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 
-		if @post.update(params[:post].permit(:title, :body))
+		if @post.update(post_params)
 			redirect_to @post
 		else
 			render 'edit'
@@ -46,7 +46,7 @@ class PostsController < ApplicationController
 	private
 
 		def post_params
-			params.require(:post).permit(:title,:body)
+			params.require(:post).permit(:title,:body,:image)
 		end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
 	belongs_to :user
 	validates :title, presence: true
 	validates :body, presence: true
+
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+
 end
